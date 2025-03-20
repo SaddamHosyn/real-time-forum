219c89493ec9d837a4ff8834e52d21e63c5f8ccb
Saddam <saddamhussain1919@gmail.com>
2025-03-21 00:16:14 +0200

    created sign in section

--- assets/js/app.js
+++ assets/js/app.js
@@ -74,4 +74,9 @@ document.getElementById('combobox-input').addEventListener('keydown', (e) => {
    }
 });
 
-
+// Function to toggle dropdown
+function toggleDropdown(event, dropdownId) {
+    event.preventDefault(); // Prevent default link behavior
+    const dropdown = document.getElementById(dropdownId);
+    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+  }
